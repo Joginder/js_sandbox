42cca72fd0eb9d655083f36df7b43cc420b9b45e
joginder <joginder.singh@srijan.net>
2019-06-02 22:31:01 +0530

    learning JavaScript

---
+++ app.js
@@ -0,0 +1,92 @@
+// Working with consol
+
+// console.log('Your Javascript works');
+// console.log(9873251584);
+// var q = 'qwerty';
+// console.log(q);
+// console.log({a:1, b:1});
+// console.clear();
+
+// // var, let, const
+
+// var name = 'Prabhleen kaur';
+// console.log(name);
+// name = 'Prabh';
+// console.log(name);
+
+// //init var
+
+// var royalenfield;
+// console.log(royalenfield);
+// royalenfield = 'classic350';
+// console.log(royalenfield);
+
+// Var can be letter, number, _, $.
+
+//LET
+
+// let name = 'Prabh';
+// console.log(name);
+// name = 'Prebhleen Kaur';
+// console.log(name);
+
+//CONST
+
+// const name = 'Joginder singh';
+// console.log(name);
+
+// const about = {
+//     name : 'Joginder',
+//     age : '29'
+// }
+
+// about.name = 'Gurpreet';
+
+// // console.log(about);
+
+// const numbers = [1,2,3,4,5,6,7,8,9];
+// numbers.push(10);
+// console.log(numbers);
+
+// PRIMITIVE
+
+//Strings
+const name = 'Joginder Singh';
+console.log(typeof name);
+
+//Numbers
+const age = 29;
+console.log(typeof age);
+
+//Boolean
+const hasKids = true;
+console.log(typeof hasKids);
+
+// Null it shows you ojbect because there is an error in JavaScript.
+const truck = null;
+console.log(typeof truck);
+
+// Undefined
+let test;
+
+// Symbol
+const sym = Symbol();
+console.log(typeof sym);
+
+
+// DATA TYPES AND OBJECTS
+
+// Arrey
+const hobbies = ['sports','gym','movies'];
+console.log(typeof hobbies);
+
+// object literl
+const address = {
+    city: 'New Delhi',
+    state: 'Delhi'
+}
+console.log(typeof address);
+
+const today = new Date();
+console.log(today);
+console.log(typeof today);
